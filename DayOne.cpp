ee41aa3ac49150c01c74efe27532b67ae61e3fb3
paoloprogram12 <sethpsalazar@gmail.com>
2025-01-06 12:12:11 -0800

    sdf

---
+++ DayOne.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    int numOne, numTwo;
+    cout << "Enter Two Numbers: ";
+    cin >> numOne >> numTwo;
+    cout << "numOne: " << numOne << " numTwo: " << numTwo << endl; 
+
+    return 0;
+}
