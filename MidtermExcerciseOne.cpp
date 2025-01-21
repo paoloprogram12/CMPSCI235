9a84460dbe7dafb60584e46cf3dce73fd7d8b0ec
paoloprogram12 <sethpsalazar@gmail.com>
2025-01-20 22:59:36 -0800

    amongus

---
+++ MidtermExcerciseOne.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numOne, numTwo, numThree;
+
+    cout << "Enter three numbers:" << endl;
+    cin >> numOne >> numTwo >> numThree;
+
+    if ((numOne > numTwo) && (numOne > numThree)) {
+        if (numTwo > numThree) {
+            cout << numThree << " " << numTwo << " " << numOne << endl;
+        }
+        else if (numThree > numTwo) {
+            cout << numTwo << " " << numThree << " " << numOne << endl;
+        }
+    }
+    else if ((numTwo > numOne) && (numTwo > numThree)) {
+        if (numOne > numThree) {
+            cout << numThree << " " << numOne << " " << numTwo << endl;
+        }
+        else if (numThree > numOne) {
+            cout << numOne << " " << numThree << " " << numTwo << endl;
+        }
+    }
+    else if ((numThree > numOne) && (numThree > numTwo)) {
+        if (numOne > numTwo) {
+            cout << numTwo << " " << numOne << " " << numThree << endl;
+        }
+        else if (numTwo > numOne) {
+            cout << numOne << " " << numTwo << " " << numThree << endl;
+        }
+    }
+
+}
---
+++ MidtermExerciseTwo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    bool grader = false;
+
+    int grade;
+    int avgCount = 0;
+    int gradesTotal = 0;
+    double avg;
+
+    cout << "Enter a grade:" << endl;
+
+    while (grader == false) {
+
+        cin >> grade;
+
+        if ((grade <= 100) && (grade >= 0)) {
+            cout << "Enter another grade:" << endl;
+            gradesTotal += grade;
+            avgCount++;
+        }
+        else if (grade == 999) {
+            avg = gradesTotal / avgCount;
+            cout << "The grade average total is: " << avg << endl;
+            break;
+        }
+        else {
+            cout << "Invalid input, try again" << endl;
+        }
+    }
+
+}
