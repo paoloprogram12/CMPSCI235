9230dc68bd3a1a0cbb980b87c0a9982ca6006fc3
paoloprogram12 <sethpsalazar@gmail.com>
2025-01-22 12:23:02 -0800

    j

---
+++ Day9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+const int MAXI = 5;
+const int MAXJ = 4;
+int i, j;
+
+for (i = 1; i <= MAXI; i++) {
+
+    cout << "i is now " << i << endl;
+
+    for (j = 1; j <= MAXJ; j++) {
+
+        cout << "j = " << j << endl;
+
+    }
+
+}
+
+}
