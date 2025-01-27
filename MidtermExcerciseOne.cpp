3ada028d08e7e38740e77418ac4989ac5ac713fd
paoloprogram12 <sethpsalazar@gmail.com>
2025-01-27 00:25:18 -0800

    sad

--- MidtermExcerciseOne.cpp
+++ MidtermExcerciseOne.cpp
@@ -8,30 +8,52 @@ int main() {
     cout << "Enter three numbers:" << endl;
     cin >> numOne >> numTwo >> numThree;
 
-    if ((numOne > numTwo) && (numOne > numThree)) {
-        if (numTwo > numThree) {
-            cout << numThree << " " << numTwo << " " << numOne << endl;
-        }
-        else if (numThree > numTwo) {
-            cout << numTwo << " " << numThree << " " << numOne << endl;
-        }
+    // if ((numOne > numTwo) && (numOne > numThree)) {
+    //     if (numTwo > numThree) {
+    //         cout << numThree << " " << numTwo << " " << numOne << endl;
+    //     }
+    //     else if (numThree > numTwo) {
+    //         cout << numTwo << " " << numThree << " " << numOne << endl;
+    //     }
+    // }
+    // else if ((numTwo > numOne) && (numTwo > numThree)) {
+    //     if (numOne > numThree) {
+    //         cout << numThree << " " << numOne << " " << numTwo << endl;
+    //     }
+    //     else if (numThree > numOne) {
+    //         cout << numOne << " " << numThree << " " << numTwo << endl;
+    //     }
+    // }
+    // else if ((numThree > numOne) && (numThree > numTwo)) {
+    //     if (numOne > numTwo) {
+    //         cout << numTwo << " " << numOne << " " << numThree << endl;
+    //     }
+    //     else if (numTwo > numOne) {
+    //         cout << numOne << " " << numTwo << " " << numThree << endl;
+    //     }
+    // }
+
+    int temp;
+
+    if (numOne > numTwo) {
+        temp = numOne;
+        numOne = numTwo;
+        numTwo = temp;
     }
-    else if ((numTwo > numOne) && (numTwo > numThree)) {
-        if (numOne > numThree) {
-            cout << numThree << " " << numOne << " " << numTwo << endl;
-        }
-        else if (numThree > numOne) {
-            cout << numOne << " " << numThree << " " << numTwo << endl;
-        }
+    if (numTwo > numThree) {
+        temp = numTwo;
+        numTwo = numThree;
+        numThree = temp;
     }
-    else if ((numThree > numOne) && (numThree > numTwo)) {
-        if (numOne > numTwo) {
-            cout << numTwo << " " << numOne << " " << numThree << endl;
-        }
-        else if (numTwo > numOne) {
-            cout << numOne << " " << numTwo << " " << numThree << endl;
-        }
+    if (numOne > numTwo) {
+        temp = numOne;
+        numOne = numTwo;
+        numTwo = temp;
     }
+
+    cout << numOne << " " << numTwo << " " << numThree << endl;
+
+
 
     return 0;
 
